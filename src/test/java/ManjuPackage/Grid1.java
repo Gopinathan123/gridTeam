1dbef9834cb3e10e521a7aedbf24ee16fb077f21
manju1702 <manjusahu1702@gmail.com>
2023-12-01 16:19:35 +0530

    my first commit

---
+++ src/test/java/ManjuPackage/Grid1.java
@@ -0,0 +1,17 @@
+package ManjuPackage;
+
+import org.testng.annotations.Test;
+
+public class Grid1 
+{
+	@Test
+	public void m1()
+	{
+		System.out.println("m1 is executed");
+	}
+	public void m2()
+	{
+		System.out.println("m2 is executed");
+	}
+
+}
